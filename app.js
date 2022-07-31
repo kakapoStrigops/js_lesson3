7f58d134f8e551b413789dcc02fc2b28df7a0355
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-31 01:58:57 +0000

    入力ダイアログの表示

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-// メイン部分
-let alertString;
-alertString = addString("WebCamp");
+// // メイン部分
+// let alertString;
+// alertString = addString("WebCamp");
 
-// 作成した関数を呼び出す
-alert(alertString);
+// // 作成した関数を呼び出す
+// alert(alertString);
 
-// 簡単な関数を作成
-function addString(strA) {
-  let addStr = "Hello " + strA;
-  return addStr;
-}
+// // 簡単な関数を作成
+// function addString(strA) {
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+
+// 複数の関数を定義
+// 入力ダイアログの表示
+let promptStr = prompt('何か好きな文字を入力してください。');
+
+alert(promptStr);
 
 
