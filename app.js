b38939b6f85a62735c2f7e181f20a7262fb0da47
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-31 01:42:58 +0000

    簡単な関数の作成・呼び出し

---
+++ app.js
@@ -0,0 +1,14 @@
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 簡単な関数を作成
+function addString(strA) {
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+
